0f9670c850242ab32e2553039ec81d2b91882fb6
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2024-09-29 13:58:25 +0500

    updated

--- server/routes/index.js
+++ server/routes/index.js
@@ -72,6 +72,7 @@ app.delete("/tasks/:id", async (req, res) => {
 	}
 });
 
+
 // Server running on port 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
