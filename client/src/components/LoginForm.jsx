9d556069527b3f636a9ed57eabaf192b484dd012
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2024-10-08 18:09:28 +0500

    Update LoginForm.jsx

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -14,7 +14,8 @@ function LoginForm({ setlogin }) {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:5000/api/users/login', {
+            // const response = await fetch('http://localhost:5000/api/users/login', {
+            const response = await fetch('https://todo-app-full-stack-opal.vercel.app/api/users/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
