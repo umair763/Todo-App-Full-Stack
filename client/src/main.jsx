74bde44f6525207d26523d69308abaa18460d4f7
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2024-11-16 14:40:44 +0500

    update 1.0

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,13 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <App />
-   </GoogleOAuthProvider>
+   <React.StrictMode>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+         <App />
+      </GoogleOAuthProvider>
+   </React.StrictMode>
 );
