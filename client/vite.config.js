28b02f6c960c10622b8101235e3265d3b6270a1a
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2024-11-16 13:17:35 +0500

    vite package update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
-  plugins: [react()],
-})
+   plugins: [react()],
+   optimizeDeps: {
+      include: ['@react-oauth/google'], // Force pre-bundling of the dependency
+   },
+});
