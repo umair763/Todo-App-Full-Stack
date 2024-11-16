5679b21272498ca9620c75b40a885c9904ae68c0
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2024-11-16 14:35:07 +0500

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,9 +5,7 @@ import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <React.StrictMode>
-      <GoogleOAuthProvider clientId={googleClientId}>
-         <App />
-      </GoogleOAuthProvider>
-   </React.StrictMode>
+   <GoogleOAuthProvider clientId={googleClientId}>
+      <App />
+   </GoogleOAuthProvider>
 );
