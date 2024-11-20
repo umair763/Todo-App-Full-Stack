aebcf7388e4e0ebf86243ee0532def24d99d609d
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2024-11-20 14:52:40 +0500

    error resolved

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-=======
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
@@ -24,4 +12,3 @@ createRoot(document.getElementById('root')).render(
       </GoogleOAuthProvider>
    </StrictMode>
 );
->>>>>>> Google-signin-integration
